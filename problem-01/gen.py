f2b11b1a1580c8204e78b719e17f53d262daef4b
htc550605125 <htc550605125@gmail.com>
2013-05-15 22:35:25 +0000

    del main

--- problem-01/gen.py
+++ problem-01/gen.py
@@ -9,25 +9,31 @@ import random
 randint = lambda a,b:str(random.randint(a,b));
 
 tasks = [
-        ['1', 10],
-        ['2', 20],
-        ['3', 50],
-        ['4', 100],
-        ['5', 200],
-        ['6', 400],
-        ['7', 800],
-        ['8', 1000],
-        ['9', 10000],
-        ['10', 20000],
+        ['1', 2, 1, 2],
+        ['2', 10, 4, 7],
+        ['3', 50, 13, 14],
+        ['4', 100, 1, 100],
+        ['5', 500, 314, 413],
+        ['6', 1000, 1, 2],
+        ['7', 5000, 4999, 5000],
+        ['8', 8000, 3514, 5137],
+        ['9', 10000, 9999, 10000],
+        ['10', 10000, 172, 9981],
         ]
 
 def gen(opt, fd):
-    fd.write(randint(1, opt[1]));
-    fd.write(' ');
-    fd.write(randint(1, opt[1]));
+    size, b0, b1 = opt[1], opt[2], opt[3]
+    fd.write(str(size)+'\n')
+    l = range(1, size+1)
+    del l[b1-1]
+    del l[b0-1]
+    random.shuffle(l)
+    for x in l:
+        fd.write(str(x)+' ')
+    fd.write('\n')
 
 print 'Remove Last Generation'
-subprocess.call('rm *.txt', shell = True)
+subprocess.call('rm data/*.txt', shell = True)
 
 print 'Generating...'
 startTime = time.time()
--- problem-01/main.cpp
+++ problem-01/main.cpp
@@ -1,15 +1,23 @@
-#include <iostream>
-#include <algorithm>
-
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<cmath>
 using namespace std;
-
 int main()
 {
-    int a, b, c, ans = 0;
-    cin>>a>>b;
-    c = max(a, b);
-    for (int i = 2; i <= c; ++i)
-        if (a % i == b % i)
-            ++ans;
-    cout<<ans<<endl;
+     int i,n;
+     scanf("%d",&n);
+     long long sum1=(1LL+n)*n/2;
+     long long sum2=1LL*n*(n+1)/2*(2*n+1)/3;
+     int x;
+     for(i=1;i<=n-2;++i)
+     {
+         scanf("%d",&x);
+         sum1-=x;
+         sum2-=x*x;
+     }    
+     int num1=(sum1+sqrt(2*sum2-sum1*sum1))/2;
+     int num2=(sum1-sqrt(2*sum2-sum1*sum1))/2;
+     printf("%d %d\n", min(num1,num2), max(num1,num2));
+     return 0;
 }
